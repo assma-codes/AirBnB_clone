44c48bcac34cfcb8a26de9336d897187176f4095
assma-codes <assma.hm89@gmail.com>
2024-03-10 01:35:57 -0800

    craeting user class

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+User class module
+"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """
+    Define user information 
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
