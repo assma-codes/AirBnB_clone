4920080eb899c826ba30276045b0d2d58bac366d
assma-codes <assma.hm89@gmail.com>
2024-03-10 17:45:05 -0700

    fix base_model pycode style

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 import models
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
@@ -24,17 +25,21 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
-        models.storage.new(self)  
+        models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
+
         self.updated_at = datetime.utcnow()
         models.storage.save()
+
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
+        """
+        Return the dictionary of the BaseModel instance.
         Includes the key/value pair __class__ representing
         the class name of the object.
-        """  
+        """
+
         inst_dic = self.__dict__
         instanc_dict = inst_dic.copy()
         instanc_dict["__class__"] = self.__class__.__name__
@@ -42,8 +47,11 @@ class BaseModel:
         instanc_dict["updated_at"] = self.updated_at.isoformat()
 
         return instanc_dict
+
     def __str__(self) -> str:
-        """Return the print/str representation of the BaseModel instance."""
+        """
+        Return the print/str representation of the BaseModel instance.
+        """
+
         class_name = self.__class__.__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}"
-    
